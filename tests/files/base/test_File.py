243c936ffde2c2fd7a302f15ac7601de9a53e965
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-09-09 01:33:27 +1000

    (files)(tests) created files for tests to go in

---
+++ tests/files/base/test_File.py
@@ -0,0 +1,10 @@
+# TODO: paramaterised tests
+# -- test every initialiser state
+# -- - from_archive
+# -- - from_bytes[BytesIO,StringIO]
+# -- - from_file[BufferedReader,TextIOWrapper]
+# -- - from_lines
+# -- - from_stream[BufferedReader,TextIOWrapper,BytesIO,StringIO]
+# -- DataType variants & input data variants
+# -- - open(..., "r") [io.TextIOWrapper] & StringIO
+# -- - open(..., "rb") [io.BufferedReader] & BytesIO
